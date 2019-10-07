9663a04c4ff1337415c58aa5a4db8a3c3b9c29ad
scottyboimorgan <54558267+scottyboimorgan@users.noreply.github.com>
2019-10-06 23:29:33 -0400

    Update Piece.java

---
+++ pieces/Piece.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public abstract class Piece{
+  private String name;
+  private Spot location;
+  private Color color;
+  
+  public Piece(Spot s, Color c, String n){
+    name = n;
+    color = c;
+    location = s;
+  }
+  public abstract Spot move();
+  
+}
