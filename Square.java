8ff15acc505708bcb4cb0241d8764a1c0e0112c6
ryandellison <ellisondryan@gmail.com>
2019-10-26 22:43:31 -0400

    Changed Spot to Square

---
+++ Square.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+
+public class Square{
+  private String locString;
+  private int locInt;
+  private Color display;
+  private Color color;
+  private boolean occupied;
+  
+  public Square(int num, String name, Color c){
+    locInt = num;
+    locString = name;
+    color = c;
+    //this is a test for committing
+  }
+  
+  public void occupy(){
+    occupied = true; 
+  }
+  
+  public void vacate(){
+    occupied = false; 
+  }
+  
+  public void highlight(){
+    if(!occupied){
+      display = Color.green;
+    }
+    else{
+      display = Color.red;
+    }
+  }
+  
+  public void unhighlight(){
+     display = color; 
+  }
+  
+  
+}
--- pieces/Piece.java
+++ pieces/Piece.java
@@ -3,14 +3,12 @@ import java.awt.Color;
 
 public abstract class Piece{
   private String name;
-  private Spot location;
   private Color color;
   
-  public Piece(Spot s, Color c, String n){
+  public Piece(Color c, String n){
     name = n;
     color = c;
-    location = s;
   }
-  public abstract Spot move();
+  public abstract Square move();
   
 }
