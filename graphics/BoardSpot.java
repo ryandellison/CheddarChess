bc32f93f296e15b1045f2757a7510db600073789
ryandellison <ellisondryan@gmail.com>
2019-11-06 20:29:49 -0500

    Moved some files around

---
+++ graphics/BoardSpot.java
@@ -0,0 +1,44 @@
+package board;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+
+@SuppressWarnings("WeakerAccess")
+public class BoardSpot extends JButton {
+
+	private static final Color LIGHT_SPOT = new Color(222, 184, 135);
+	private static final Color DARK_SPOT = new Color(139, 69, 19);
+	private Font font;
+	private boolean isEmpty;
+	private String piece;
+
+	BoardSpot(String piece)
+	{
+		super(piece);
+		this.piece = piece;
+		isEmpty = piece.isEmpty(); // refers to if the spot has a piece in it or not.
+		font = new Font("Ariel", Font.PLAIN, 35);
+
+	}
+
+	void set(int index, int row)
+	{
+		setFont(font);
+		setOpaque(true);
+		setBackground((index+row)%2 == 0 ? DARK_SPOT : LIGHT_SPOT);
+		setHorizontalAlignment(SwingConstants.CENTER );
+	}
+
+	public String getPiece()
+	{
+		return piece;
+	}
+
+	boolean isEmpty()
+	{
+		return isEmpty;
+	}
+
+}
