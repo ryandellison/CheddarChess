d01dc170acc56037374f35020c07f02efd0d2e70
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2019-11-13 12:26:31 -0500

    Went back to original

--- constant/Colors.java
+++ constant/Colors.java
@@ -10,15 +10,9 @@
 
 package constant;
 
-import java.awt.*;
-
 public final class Colors
 {
 	public final static boolean LIGHT = true;
 	public final static boolean DARK = false;
-
-	public static final Color LIGHT_SPOT = new Color(222, 184, 135);
-	public static final Color DARK_SPOT = new Color(139, 69, 19);
-	public static final Color HIGHLIGHTED_SPOT = new Color(50, 205, 50);
 }
 
