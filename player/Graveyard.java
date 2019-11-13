09b9c216cd45c867e87662a73f59ccfacb63a94f
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2019-11-13 12:59:30 -0500

    Added graveyard Logic

--- player/Graveyard.java
+++ player/Graveyard.java
@@ -1,5 +1,8 @@
 package player;
 import pieces.*;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Graveyard
@@ -10,6 +13,7 @@ public class Graveyard
 	public final static int BISHOP_VALUE = 4;
 	public final static int QUEEN_VALUE = 8;
 
+	private int numPieces = 0;
 	private ArrayList<Piece> graveyard;
 
 	public Graveyard()
@@ -20,11 +24,38 @@ public class Graveyard
 	public void addToGraveyard(Piece p)
 	{
 		graveyard.add(p);
+		numPieces++;
 	}
 
-	public ArrayList<Piece> getGraveyardPieces()
+	public int getNumPieces()
 	{
-		return graveyard;
+		return numPieces;
 	}
+
+	public JButton[] getPieces(Player player)
+	{
+		JButton[] capturedPieces = new JButton[numPieces];
+		for(int i = 0; i < numPieces; i++){
+			capturedPieces[i]= getCapturedPiece(graveyard.get(i));
+			capturedPieces[i].setBackground(getPlayerColor(player));
+		}
+		return capturedPieces;
+	}
+
+	private Color getPlayerColor(Player player)
+	{
+		return player.getPlayerNum().equals("Player 1") ? new Color(222, 184, 135)
+				: new Color(180, 79, 20);
+	}
+
+	private JButton getCapturedPiece(Piece piece)
+	{
+		JLabel pieceImage = new JLabel(piece.getUnicode());
+		JButton button = new JButton(piece.getUnicode());
+		button.setFont(new Font("Ariel", Font.PLAIN, 50));
+		return button;
+	}
+
+
 
 }
