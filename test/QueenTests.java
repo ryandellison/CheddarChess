d23653d715f82fec784bd68500c006a867702e25
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2019-11-25 11:01:32 -0500

    Queen Tests file added

---
+++ test/QueenTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import board.Board;
+import org.junit.Before;
+import org.junit.Test;
+
+import pieces.Moves;
+import pieces.Pair;
+import pieces.Queen;
+
+import static org.junit.Assert.*;
+
+/**
+ * Collection of test cases for pawn
+ */
+public class QueenTests
+{
+
+
+}
+
