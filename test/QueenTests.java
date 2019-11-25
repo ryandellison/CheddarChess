07549ef9ab23bfccf6bc478606b336e3aa480353
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2019-11-25 11:11:22 -0500

    Added Skeleton code for both pieces

--- test/QueenTests.java
+++ test/QueenTests.java
@@ -11,11 +11,19 @@ import pieces.Queen;
 import static org.junit.Assert.*;
 
 /**
- * Collection of test cases for pawn
+ * Collection of test cases for queen
  */
 public class QueenTests
 {
+    private Board board;
+    private Queen testPawn;
 
+    @Before
+    public void initializeBoard()
+    {
+        board = new Board(true);
+        testPawn = new Queen(true,"Queen");
+    }
 
 }
 
