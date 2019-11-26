35a7867f1b3d260298394596848b80917fccc614
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2019-11-25 21:14:00 -0500

    More tests for queen

--- test/QueenTests.java
+++ test/QueenTests.java
@@ -78,16 +78,68 @@ public class QueenTests
             if(i != 4)
                 assertTrue(calculatedMoves.contains(new Pair(i,3))); // up down
         }for(int i = 0; i < 8; i++){
-            if(i != 3)
-                assertTrue(calculatedMoves.contains(new Pair(4,i))); // left to right
-        }for(int i = 0; i < 8; i++){
-            if(i != 4 && j != 4)
-                assertTrue(calculatedMoves.contains(new Pair(i,--j))); // diag tr to bl
-        }
+        if(i != 3)
+            assertTrue(calculatedMoves.contains(new Pair(4,i))); // left to right
+    }for(int i = 0; i < 8; i++){
+        if(i != 4 && j != 4)
+            assertTrue(calculatedMoves.contains(new Pair(i,--j))); // diag tr to bl
+    }
         j = -1;
         for(int i = 1; i < 8; i++){
             if(i != 4 && j != 3)
                 assertTrue(calculatedMoves.contains(new Pair(i,++j))); // diag tl to br
+        }
+
+    }
+
+    /**
+     * Tests queen in the corner with available pieces that can be captured
+     */
+    @Test
+    public void testQueenWithPiecesInMiddle()
+    {
+        int j = 8;
+        board.getSquare(0,0).setPiece(darkQueen);
+        board.getSquare(3,3).setPiece(new Rook(true,"Rook"));
+        board.getSquare(6,0).setPiece(new Knight(true,"Knight"));
+        board.getSquare(0,5).setPiece(new Bishop(true,"Bishop"));
+
+
+        calculatedMoves = board.getValidMoves(new Pair(0,0));
+
+        assertEquals(14,calculatedMoves.getSize());
+
+        for(int i = 1; i < 7; i++) {
+            assertTrue(calculatedMoves.contains(new Pair(i, 0))); //  straight down no pieces
+        }
+        for(int i = 1; i < 6;i++) {
+            assertTrue(calculatedMoves.contains(new Pair(0, i))); // across with a piece
+        }
+        for(int i = 1; i < 4; i++) {
+            assertTrue(calculatedMoves.contains(new Pair(i, i))); // diag with a piece
+        }
+    }
+
+    /**
+     * Test queen going diagonal left and right only from starting position
+     */
+    @Test
+    public void testQueenMoves()
+    {
+        board.getSquare(0,2).setPiece(new Bishop(false,"Bishop")); // left of queen
+        board.getSquare(0,3).setPiece(darkQueen);
+        board.getSquare(0,4).setPiece(new King(false,"King")); // right of queen
+        board.getSquare(1,3).setPiece(new Pawn(false,"Pawn")); // front of queen
+
+        calculatedMoves = board.getValidMoves(new Pair(0,3));
+        assertEquals(7,calculatedMoves.getSize());
+        int j = 3;
+        for(int i = 1; i < 4; i++){ // diag left
+            assertTrue(calculatedMoves.contains(new Pair(i,--j)));
+        }
+        j = 3;
+        for(int i = 1; i < 4; i++){ // diag right
+            assertTrue(calculatedMoves.contains(new Pair(i,++j)));
         }
 
     }
